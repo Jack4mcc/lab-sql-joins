dfd68ab474332a1540e9ac1f99cca29e052d5375
Jack <jack4mccgmail.comgit config --global user.email jack4mccgmail.com>
2024-12-22 11:10:28 +0100

    week 4 lab 4 done

---
+++ Lab 4 Joins.sql
@@ -0,0 +1,46 @@
+USE sakila;
+
+SELECT *
+FROM film f
+LEFT JOIN film_category c
+ON f.film_id = c.film_id;
+
+
+SELECT c.category_id AS film_category_id, COUNT(f.film_id) AS number_of_films
+FROM film f
+LEFT JOIN film_category c
+ON f.film_id = c.film_id
+GROUP BY c.category_id
+ORDER BY number_of_films DESC;
+
+SELECT store_id, country, city
+From address a
+RIGHT JOIN store s
+ON a.address_id = s.address_id
+LEFT JOIN city c
+ON a.city_id = c.city_id
+LEFT JOIN country r
+ON c.country_id = r.country_id;
+
+SELECT *
+FROM store;
+
+SELECT *
+fROM payment;
+
+SELECT store_id, SUM(amount) AS total_revenue
+From payment p
+INNER JOIN staff s
+ON p.staff_id = s.staff_id
+GROUP BY store_id;
+
+SELECT *
+FROM film_category;
+
+
+SELECT ROUND(AVG(length),2) AS avg_length, category_id
+From film f
+INNER JOIN film_category c
+ON f.film_id = c.film_id
+GROUP BY category_id
+ORDER BY avg_length DESC;
